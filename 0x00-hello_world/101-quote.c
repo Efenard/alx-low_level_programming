2dc7592e5d786c4764951020370f19dcab83e8f1
Efenard <leewainston@yahoo.com>
2022-06-30 16:21:29 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Description: 'UNIX is basically a simple operating system'
+*
+* Return: 1 if Success
+*/
+
+int main(void)
+{
+
+	printf("and that piece of art is useful\n - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
