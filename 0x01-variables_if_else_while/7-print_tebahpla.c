c8f184fd8cde47a26e720887ffe4f5757f4b1465
Efenard <leewainston@yahoo.com>
2022-07-01 20:45:42 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: '7-print_tebahpla'
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	char al;
+
+	for (al = 'z'; al >= 'a'; al--)
+	putchar(al);
+	putchar('\n');
+
 	/* your code goes there */
 	return (0);
 }
