4afa10f01507d1a3391356c4a101b5d8e5e76c4b
Efenard <leewainston@yahoo.com>
2022-07-01 20:28:38 -0700

    Print

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: '5 Print numbers'
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	int num1;
+	
+	for (num1 = 0; num1 < 10; num1++)
+	printf("%d", num1);
+	printf("\n");
+}
 	/* your code goes there */
 	return (0);
 }
