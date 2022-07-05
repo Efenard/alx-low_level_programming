4c9ab55b4c0dc8036fe987cc0c9e7c662329eae6
Efenard <leewainston@yahoo.com>
2022-07-05 04:59:14 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: 'script that print _putchar'
+ *
+ * Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+
+}
