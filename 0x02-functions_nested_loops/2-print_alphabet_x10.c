0b17f2afc7afafa54fd870d02c2fcb45bef4cbc2
Efenard <leewainston@yahoo.com>
2022-07-05 20:48:59 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ * File: 2 print_alphabetx10
+ *
+ *
+ *
+ * Return: 0
+ *
+ **/
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char c;
+	while (count++ <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN.H
-#define MAIN.H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -16,4 +14,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
-#endif
+
