8c4c5e34f47ec02638bd060d66141338ac52bef9
Efenard <leewainston@yahoo.com>
2022-07-12 11:51:23 -0700

    print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,9 @@ void print_rev(char *s)
 {
 	int len = 0, index;
 
-	while (*s[index++])
+	while (s[index++])
 		len++;
 	for (index = len - 1; index >= 0; index--)
-		putchar(*s[index]);
+		_putchar(s[index]);
 	putchar('\n');
 }
