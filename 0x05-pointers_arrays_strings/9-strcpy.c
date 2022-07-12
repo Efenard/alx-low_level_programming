7fdb7659029e2e02368b5b1ddddb1bda5571da32
Efenard <leewainston@yahoo.com>
2022-07-12 16:24:32 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,10 +14,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
-
-	for (src[index] != '\0'; index++)
+	
+	for(; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
 	}
-	return (dest);
+	return (*dest);
 }
