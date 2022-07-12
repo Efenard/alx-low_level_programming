be1c3caef6d914d99937fa476d43007c061da1f5
Efenard <leewainston@yahoo.com>
2022-07-12 11:47:15 -0700

    print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,7 +14,7 @@ void print_rev(char *s)
 
 	while (s[index++])
 		len++;
-	for (index = len - 1; index >= 0; index --)
+	for (index = len - 1; index >= 0; index--)
 		putchar(s[index]);
 	putchar('\n');
 }
