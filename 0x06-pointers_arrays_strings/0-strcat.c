d8697cfe7687012d290a9915b65e55780d12be1c
Efenard <leewainston@yahoo.com>
2022-07-14 15:53:05 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 
 /*
  *
- * *_strcat(char *dest, char *src) - Function that concatenates two strings.
+ * *_strcat(char *dest, char *src) - Function that concatenates two strings
  *
  * @dest: Destination String
  *
@@ -13,8 +13,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char str1;
-	
-	str1 = _strcat(dest, src);
+	int index = 0, dest_len = 0;
+
+	wbile (dest[index])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 	
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio/h>
+
+/*
+ *
+ * *_strcat(char *dest, char *src) - Function that concatenates two strings.
+ *
+ * @dest: Destination String
+ *
+ * @src: Source String
+ *
+ * */
+
+char *_strcat(char *dest, char *src)
+{
+	char str1;
+	
+	str1 = _strcat(dest, src);
+	
+}
