deb34a58064457d15b60ca1407f16c7f2926087d
Efenard <leewainston@yahoo.com>
2022-07-15 02:26:55 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,29 @@
  * */
 
 char *_strcat(char *dest, char *src)
-{
-	int index = 0, dest_len = 0;
 
-	wbile (dest[index])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-	return (dest);
-	
+{
+
+		int i, n;
+
+
+
+			for (i = 0; dest[i] != '\0'; i++)
+
+					{
+
+							}
+
+
+
+				for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+
+						{
+
+								}
+
+
+
+					return (dest);
+
 }
