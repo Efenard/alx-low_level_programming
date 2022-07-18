e85e85fbdfcd6131f75cdcd267e159388344b80e
Efenard <leewainston@yahoo.com>
2022-07-18 11:27:46 -0700

    memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /*
- *
  * _memset - Fill memory with a constant byte
- *
  * @s: memory area to fill
- *
  * @b: constant byte to fill
- *
  * @n: bytes of memory area to fill
  *
  * Return: the memory area filled
