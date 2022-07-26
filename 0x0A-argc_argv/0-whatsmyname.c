e568fa5523af2d825f1f957413d0fad3fedb29f8
Efenard <leewainston@yahoo.com>
2022-07-26 13:53:27 -0700

    add

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,37 +1,20 @@
 #include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
-/**
+/*
+ * main - program that prints its name, followed by a new line
  *
- *  * main - program that prints its name, followed by a new line
+ * @argc: argument count for main
  *
- *   *
+ * @argv: vector to the arguments
  *
- *    * @argc: argument count for main
- *
- *     * @argv: vector to the arguments
- *
- *      *
- *
- *       * Return: void
- *
- *        */
-
-
+ * Return: void
+ * */
 
 int main(int argc, char **argv)
-
 {
-
-		if (argc >= 1)
-
-					printf("%s\n", argv[0]);
-
-			return (0);
-
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	return (0);
 }
